a6153c30653acbd0796c713083401ed9afeac8d2
Borshtsch <max1238no1@gmail.com>
2026-03-25 20:45:32 +0200

    Slight changes to the code.

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,32 @@
 #include <fstream>
 #include <stdint.h>
 #include <longstr.h>
+#include <unordered_map> 
 
-class Flag {
+enum eFlag {
+	eHelp,
+	eFile,
+	eId,
+	eFind_name,
+	eCondition
+};
+
+typedef std::unordered_map<eFlag, std::string> uMap;
+
+class Flag {//I dont know what to do with this:
+	uMap _flagmap = {
+		{eHelp,		 "--help"},
+		{eFile,		 "--file"},
+		{eId,        "-id"},
+		{eFind_name, "-find-name"},
+		{eCondition, "--condition"}
+	};
 	std::string _flags[10][2] = {
-	{"--help" , "-h"},
-	{"--file" , "-f"}
+		{"--help"		, "-h"},
+		{"--file"		, "-f"},
+		{"-id"			, ""},
+		{"-find-name"	, ""},
+		{"--condition"	, "-c"}
 	};
 	std::vector<std::string> _temp;
 public:
@@ -17,18 +38,25 @@ public:
 	void push_argv(int argc, char* argv[]) {
 		for (int i = 1; i < argc; i++) {
 			_temp.push_back(argv[i]);
-			if (_temp[i - 1].front() != '-' && (check_file_flag(_temp[i - 2]) || i == 1)) {
-				std::cout << "ERROR\n";
+			if (_temp[i - 1].front() != '-' && (i == 1 || !check_file_flag(_temp[i - 2]))) {
+				std::cout << "Cannot understand meaning of {" << _temp[i - 1] << "}\n\tTry using -f or --file for files.\n";
 				return;
 			}
 		}
+	}
+	void apply_flags(){
+		
 	}
 	void out_temp() {
 		for (auto v : _temp)
 			std::cout << v << '\n';
 	}
+	void print() {
+		for (auto i : _flagmap)
+			std::cout << i.first << " " << i.second << std::endl;
+	}
 };
-
+// std::ifstream is to READ from files
 Flag flagobj;
 
 std::string Read_from_file();
@@ -36,47 +64,21 @@ void Write_to_file();
 
 int main(int argc, char* argv[]) {
 	std::string item = "";
-
+	// If you wasn't in mood to write help
+	flagobj.print();
 	if (argc == 1) {
 		std::cout <<LongString::_Help;
 		return 0;
 	}
 	flagobj.push_argv(argc, argv);
-	flagobj.out_temp();
 
+	std::ifstream a("DATssA.csv");
 	
-
 	for (int i = 1;i < argc ;i++) {
 		
 		
 	}
 
-	/*for (int i = 1; i < argc; i++)
-	{
-		std::cout << ("%s", argv[i]) << '\n';
-	}/*
-	
-
-	/*
-	
-	std::getline(std::cin, temp);
-
-	std::cout << temp << '\n';
-
-	while (true){
-		std::cout << "Read or Write: ";
-		std::cin >> item;
-
-		if (item == "Read")
-			Read_from_file();
-		else if (item == "Write") {
-			Write_to_file();
-		}
-		else if (item == "Exit")
-			break;
-		else std::cout << "Unknown command";
-	}
-	*/
 	return 0;
 }
 
