6e0c853ed74f2a65789846e7494cbe8e0906673d
Borshtsch <max1238no1@gmail.com>
2026-04-15 23:21:39 +0300

    Started using CLI11 and made basic page system

--- Source.cpp
+++ Source.cpp
@@ -1,106 +1,59 @@
-#include "Zomboid_CLI.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <stdint.h>
+#include "CLI11.hpp"
 
-const std::string Zomboid_Version = "Zomboid 0.3 (30.03.2026)";
+const std::string Zomboid_Version = "Zomboid 0.4.2 (15.04.2026)";
+const unsigned short page_size = 25;
 
-enum eFlags {
-	eHelp,
-	eFile,
-	eId,
-	eFind_name,
-	eCondition,
-	ePage,
-	eVersion
-};
-
-typedef std::unordered_map<std::string, eFlags> uMap;
-
-std::vector<std::string> Read_from_file(std::string inpfilename = "Data.csv", std::string page = "1");
-
-class Flag {//I dont know what to do with this:
-	uMap _flagmap = {
-		{"--help",		eHelp},
-		{"-h",			eHelp},
-		{"--file",		eFile,},
-		{"-f",			eFile,},
-		{"-id",			eId},
-		{"-find-name",	eFind_name},
-		{"--condition", eCondition},
-		{"-c",			eCondition},
-		{"--page",		ePage},
-		{"-p",			ePage},
-		{"--version",	eVersion},
-		{"-v",			eVersion}
-	};
-	std::vector<std::string> _temp;
-	eFlags _flagenum = eHelp;
-public:
-	void push_argv(int argc, char* argv[]) {
-		for (int i = 1; i < argc; i++) {
-			_temp.push_back(argv[i]);
-			if (_temp[i - 1].front() != '-' && (i == 1 || _flagmap[_temp[i - 2]] != eFile)) {
-				std::cerr << "Cannot understand meaning of {" << _temp[i - 1] << "}\n\tTry using -f or --file for files.\n";
-				return;
-			}
-		}
-	}
-	void apply_flags(int argc = 3){
-		_flagenum = _flagmap[_temp[0]];
-		switch (_flagenum) {
-		case 0:
-			std::cout << LongString::_Help;
-			break;
-		case 1:
-			(argc == 5) ? Read_from_file(_temp[1], _temp[3]) : Read_from_file(_temp[1]);
-			break;
-		case 2:
-			std::cout << "Id\n";
-			break;
-		case 3:
-			std::cout << "Find-name\n";
-			break;
-		case 4:
-			std::cout << "Condition\n";
-			break;
-		case 6:
-			std::cout << Zomboid_Version;
-			break;
-		}
-	}
-	
-	void out_temp() {
-		for (auto v : _temp)
-			std::cout << v;
-	}
-};
-
-Flag flagobj;
-
+void Read(std::string filename) {
+	std::cout << filename;
+}
 
 int main(int argc, char** argv) {
 
-	// If you wasn't in the mood to write --help
-	if (argc == 1) {
-		std::cout <<LongString::_Help;
-		return 0;
-	}
+	CLI::App app{ Zomboid_Version };
+	app.require_subcommand(1);
 
-	flagobj.push_argv(argc, argv);
+	argv = app.ensure_utf8(argv);
 
-	flagobj.apply_flags(argc);
+	auto Read = app.add_subcommand("read", "Reads from file");
+	//	TODO: Change for something like std::filesystem::path
+	std::string filename = "default";
+
+	Read->add_option("-f,--file", filename, "Takes file to read");
+	//Read->add_option("-d,--dir", filename, "Takes directory to read"); I'm not doing that Twin \/
 	
+	unsigned short page = 1;
+
+	Read->add_option("-p,--page", page, "Page of your file");
+
+	// Callback for using read subcommand
+	Read->callback([&]() {
+		std::ifstream file(filename);
+		
+		if (!file.is_open()) {//Check for right file
+			std::cerr << "Cannot open file:\n\t" << filename;
+			return -1;
+		}
+
+
+		std::cout << "Page: " << page << '\n';
+		std::string content = "";
+		// Reads from file using std::getline and inputs from needed page TODO: try to use .get() func
+		for (int i = 0; i <= page_size * page; i++) {
+			std::getline(file, content);
+			if (i > page_size * (page - 1))
+				std::cout << content << '\n';
+		}
+
+		file.close();
+		//std::cout << "Input was: \n\t" << filename;
+	});
+
+
+	CLI11_PARSE(app, argc, argv);
 	return 0;
 }
 
-std::vector<std::string> Read_from_file(std::string inpfilename, std::string page) {
-	std::vector<std::string> item;
-	//std::cout <<'\n' << inpfilename << '\n';
-	std::ifstream file(inpfilename);
-
-	if (!file.is_open()) {
-		std::cerr << "ERROR. Cannot open file";
-		return item;
-	}
-
-	file.close();
-	return item;
-}
